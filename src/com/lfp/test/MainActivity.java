557e486699b546a3aecebf60ff372eabeb1d3cec
linfp <linfengpu.qq.com>
2013-08-20 17:40:13 +0800

    添加编辑框与按钮

--- src/com/lfp/test/MainActivity.java
+++ src/com/lfp/test/MainActivity.java
@@ -1,15 +1,40 @@
 package com.lfp.test;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private EditText editText_getName;
+	
+	private Button button_Login;
+	
+	private TextView textView_showLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        editText_getName = (EditText)findViewById(R.id.editText_getName);
+        
+        button_Login = (Button)findViewById(R.id.button_login);
+        
+        textView_showLogin = (TextView)findViewById(R.id.textView_showLogin);
+        
+        button_Login.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		textView_showLogin.setText("hello"+editText_getName.getText());
+        	}
+        	
+        });
     }
 
 
