687859dac6c4e23778543824c6f4b65e313b6463
linfp <linfengpu.qq.com>
2014-01-24 18:14:57 +0800

    计算机应用

---
+++ src/com/lfp/calculator/CalculatorActivity.java
@@ -0,0 +1,17 @@
+package com.lfp.calculator;
+
+import com.lfp.test.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calculator);
+	}
+
+}
--- src/com/lfp/test/MainActivity.java
+++ src/com/lfp/test/MainActivity.java
@@ -1,14 +1,36 @@
 package com.lfp.test;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.lfp.calculator.CalculatorActivity;
 
 public class MainActivity extends Activity {
+
+	private Button buttonCalculator = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		buttonCalculator = (Button) findViewById(R.id.button_calculator);
+		buttonCalculator.setOnClickListener(new ButtonCalculatorListener());
+	}
+
+	class ButtonCalculatorListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent();
+			intent.setClass(MainActivity.this, CalculatorActivity.class);
+			MainActivity.this.startActivity(intent);
+
+		}
 
 	}
 }
