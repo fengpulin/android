8304d2033485182534dcbd9ac072819329e8f88a
linfp <linfengpu.qq.com>
2014-03-04 17:01:58 +0800

    完善计算器应用

--- src/com/lfp/calculator/CalculatorActivity.java
+++ src/com/lfp/calculator/CalculatorActivity.java
@@ -8,10 +8,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.lfp.test.R;
+import com.lfp.tools.StringTools;
 
 public class CalculatorActivity extends Activity {
 
-	private TextView textView_input = null;
+	private TextView textView_inputNumber = null;
+	private TextView textView_expressions = null;
 	private Button button_zero = null;
 	private Button button_one = null;
 	private Button button_two = null;
@@ -22,16 +24,33 @@ public class CalculatorActivity extends Activity {
 	private Button button_seven = null;
 	private Button button_eight = null;
 	private Button button_nine = null;
-	private String input = "";
-	//private String output = "";
+	private Button button_point = null;
+	private Button button_add = null;
+	private Button button_sub = null;
+	private Button button_mul = null;
+	private Button button_div = null;
+	private Button button_equal = null;
+	private Button button_clear = null;
+	private String inputNumber = "";
+	private String operand = "";
+	private String expressions = "";
+	private boolean inputStart = true;
+	private Operator operator = null;
+
+	private enum Operator {
+		ADD, SUB, MUL, DIV
+
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calculator);
-		textView_input = (TextView) findViewById(R.id.textView_input);
-		input = textView_input.getText().toString();
+		textView_inputNumber = (TextView) findViewById(R.id.textView_inputNumber);
+		textView_inputNumber.setText(inputNumber);
+		textView_expressions = (TextView) findViewById(R.id.textView_expressions);
+		textView_expressions.setText(expressions);
 		button_zero = (Button) findViewById(R.id.button_zero);
 		button_zero.setOnClickListener(new ButtonZeroListener());
 		button_one = (Button) findViewById(R.id.button_one);
@@ -52,25 +71,172 @@ public class CalculatorActivity extends Activity {
 		button_eight.setOnClickListener(new ButtonEightListener());
 		button_nine = (Button) findViewById(R.id.button_nine);
 		button_nine.setOnClickListener(new ButtonNineListener());
-
+		button_point = (Button) findViewById(R.id.button_point);
+		button_point.setOnClickListener(new ButtonPointListener());
+		button_add = (Button) findViewById(R.id.button_add);
+		button_add.setOnClickListener(new ButtonAddListener());
+		button_sub = (Button) findViewById(R.id.button_sub);
+		button_sub.setOnClickListener(new ButtonSubListener());
+		button_mul = (Button) findViewById(R.id.button_mul);
+		button_mul.setOnClickListener(new ButtonMulListener());
+		button_div = (Button) findViewById(R.id.button_div);
+		button_div.setOnClickListener(new ButtonDivListener());
+		button_equal = (Button) findViewById(R.id.button_equal);
+		button_equal.setOnClickListener(new ButtonEqualListener());
+		button_clear = (Button) findViewById(R.id.button_clear);
+		button_clear.setOnClickListener(new ButtonClearListener());
 	}
 
-	class ButtonPointListener implements OnClickListener{
+	class ButtonClearListener implements OnClickListener {
+		@Override
+		public void onClick(View arg0) {
+			inputNumber = "0";
+			operand = "0";
+			expressions = "";
+			inputStart = true;
+			operator = null;
+			textView_inputNumber.setText("0");
+			textView_expressions.setText("");
+		}
+	}
+
+	class ButtonEqualListener implements OnClickListener {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			
+			inputStart = true;
+			if (StringTools.isNullOrEmpty(inputNumber)) {
+				inputNumber = "0";
+			}
+			if (StringTools.isNullOrEmpty(operand)) {
+				operand = "0";
+			}
+
+			String result = "0";
+			if (!StringTools.isNullOrEmpty(operand)
+					&& !StringTools.isNullOrEmpty(inputNumber)
+					&& operator != null) {
+				switch (operator) {
+				case ADD:
+					result = String.valueOf(StringTools.strToNum(operand)
+							+ StringTools.strToNum(inputNumber));
+					break;
+				case SUB:
+					result = String.valueOf(StringTools.strToNum(operand)
+							- StringTools.strToNum(inputNumber));
+					break;
+				case MUL:
+					result = String.valueOf(StringTools.strToNum(operand)
+							* StringTools.strToNum(inputNumber));
+					break;
+				case DIV:
+					result = String.valueOf(StringTools.strToNum(operand)
+							/ StringTools.strToNum(inputNumber));
+					break;
+				default:
+					break;
+				}
+			}
+			operand = result;
+			textView_inputNumber.setText(result);
+			textView_expressions.setText("");
 		}
-		
 	}
+
+	class ButtonAddListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			inputNumber = textView_inputNumber.getText().toString();
+			operator = Operator.ADD;
+			inputStart = true;
+			operand = inputNumber;
+			expressions = operand + "+";
+			textView_inputNumber.setText(inputNumber);
+			textView_expressions.setText(expressions);
+		}
+	}
+
+	class ButtonSubListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			inputNumber = textView_inputNumber.getText().toString();
+			operator = Operator.SUB;
+			inputStart = true;
+			operand = inputNumber;
+			expressions = operand + "-";
+			textView_inputNumber.setText(inputNumber);
+			textView_expressions.setText(expressions);
+		}
+	}
+
+	class ButtonMulListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			inputNumber = textView_inputNumber.getText().toString();
+			operator = Operator.MUL;
+			inputStart = true;
+			operand = inputNumber;
+			expressions = operand + "*";
+			textView_inputNumber.setText(inputNumber);
+			textView_expressions.setText(expressions);
+		}
+	}
+
+	class ButtonDivListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			inputNumber = textView_inputNumber.getText().toString();
+			operator = Operator.DIV;
+			inputStart = true;
+			operand = inputNumber;
+			expressions = operand + "/";
+			textView_inputNumber.setText(inputNumber);
+			textView_expressions.setText(expressions);
+		}
+	}
+
+	class ButtonPointListener implements OnClickListener {
+
+		@Override
+		public void onClick(View arg0) {
+			if (inputStart == false) {
+				if (StringTools.isNullOrEmpty(inputNumber)) {
+					inputNumber = "0";
+				}
+				if (!StringTools.isInt(inputNumber)) {
+					return;
+				}
+
+				inputNumber += ".";
+			} else {
+				inputNumber = "0.";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
+		}
+
+	}
+
 	class ButtonZeroListener implements OnClickListener {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "0";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				if (StringTools.isInt(inputNumber)
+						&& Double.compare(StringTools.strToNum(inputNumber),
+								0.0) == 0) {
+					return;
+				}
+				inputNumber += "0";
+			} else {
+				inputNumber = "0";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -79,9 +245,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "1";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "1";
+			} else {
+				inputNumber = "1";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -90,9 +260,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "2";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "2";
+			} else {
+				inputNumber = "2";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -101,9 +275,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "3";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "3";
+			} else {
+				inputNumber = "3";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -112,9 +290,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "4";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "4";
+			} else {
+				inputNumber = "4";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -123,9 +305,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "5";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "5";
+			} else {
+				inputNumber = "5";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -134,9 +320,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "6";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "6";
+			} else {
+				inputNumber = "6";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -145,9 +335,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "7";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "7";
+			} else {
+				inputNumber = "7";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -156,9 +350,13 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "8";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "8";
+			} else {
+				inputNumber = "8";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
@@ -167,10 +365,15 @@ public class CalculatorActivity extends Activity {
 
 		@Override
 		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			input += "9";
-			textView_input.setText(input);
+			if (inputStart == false) {
+				inputNumber += "9";
+			} else {
+				inputNumber = "9";
+				inputStart = false;
+			}
+			textView_inputNumber.setText(inputNumber);
 		}
 
 	}
+
 }
--- src/com/lfp/tools/StringTools.java
+++ src/com/lfp/tools/StringTools.java
@@ -2,8 +2,40 @@ package com.lfp.tools;
 
 public class StringTools {
 
-	public static boolean isNumber(char c){
-		if((c-'0')>=0&&(c-'0')<=9){
+	public static boolean isNumber(char c) {
+		if ((c - '0') >= 0 && (c - '0') <= 9) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isOperator(char c) {
+		if (c == '+' || c == '-' || c == '*' || c == '/') {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isNullOrEmpty(String str) {
+		if (null != str && str.length() != 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public static double strToNum(String str) {
+		if (!isNullOrEmpty(str)) {
+			return Double.valueOf(str);
+		}
+		return 0.0;
+	}
+
+	public static boolean isInt(String str) {
+		if (!isNullOrEmpty(str)) {
+			for (int i = 0; i < str.length(); i++) {
+				if (str.charAt(i) == '.')
+					return false;
+			}
 			return true;
 		}
 		return false;
